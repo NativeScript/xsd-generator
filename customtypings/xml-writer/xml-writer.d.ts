f0492f70f4c143041dd2c55fb3ba9b76e5c15ce8
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-07-20 16:55:15 +0300

    Fix compilation errors: xml-writer return types; typescript declaration file update

--- customtypings/xml-writer/xml-writer.d.ts
+++ customtypings/xml-writer/xml-writer.d.ts
@@ -1,12 +1,12 @@
 declare module "xml-writer" {
-    export function startDocument();
-    export function startElement(name: string);
-    export function startElementNS(prefix: string, name: string, uri: string);
-    export function writeAttribute(name: string, value: any);
-    export function writeAttributeNS(prefix: string, name: string, uri: string, value: any);
-    export function writeCData(content: string);
-    export function text(text: string);
-    export function endElement();
-    export function endDocument();
+    export function startDocument(): void;
+    export function startElement(name: string): void;
+    export function startElementNS(prefix: string, name: string, uri: string): void;
+    export function writeAttribute(name: string, value: any): void;
+    export function writeAttributeNS(prefix: string, name: string, uri: string, value: any): void;
+    export function writeCData(content: string): void;
+    export function text(text: string): void;
+    export function endElement(): void;
+    export function endDocument(): void;
     export function toString(): string;
 }
